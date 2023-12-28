a8e8d95bb5e650b6e62c2d81443acf4d299f6faf
Jules <jules.dempt@outlook.fr>
2023-12-28 16:53:26 +0100

    feat: add ORM and fix node bugs

--- packages/backend/src/db/connection.ts
+++ packages/backend/src/db/connection.ts
@@ -1,1 +1,35 @@
 // Setup db connection
+
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+
+// Use .env file in development
+
+const AppDataSource = new DataSource({
+  type: 'mariadb',
+  host: process.env.DB_HOST || 'localhost',
+  port: (process.env.DB_PORT || 3306) as number,
+  username: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'lagence',
+  synchronize: process.env.NODE_ENV === 'production' ? false : true,
+  logging: false,
+  entities: ['src/models/*.ts'],
+  migrationsTableName: 'migrations',
+  migrations: ['migrations/*.ts'],
+  subscribers: [],
+});
+
+export const initializeDatabase = async () => {
+  try {
+    const conn = await AppDataSource.initialize();
+
+    console.log(
+      `Database connection success. Database: '${conn.options.database}'`
+    );
+  } catch (err) {
+    console.error('Database connection error: ', err);
+  }
+};
+
+export default AppDataSource;
