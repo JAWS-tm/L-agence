50d10b43761c7fe063ae95373484f4cbf82be00b
Hugo <hugo.dumartin@hotmail.fr>
2024-01-11 19:29:20 +0100

    frontend: add content container

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -9,23 +9,24 @@ import ProtectedRoute from './user/ProtectedRoute.tsx';
 import Login from './views/Login/Login.tsx';
 import Register from './views/Register/Register.tsx';
 import Profile from './views/Profile/Profile.tsx';
-import Contact from './components/Contact/Contact.tsx'
+import Contact from './components/Contact/Contact.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/espaceperso" element={<EspacePerso />} />
-        <Route path="/contact" element={<Contact />} />
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="/profile" element={<Profile />} />
-        </Route>
-      </Routes>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/espaceperso" element={<EspacePerso />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </div>
     </BrowserRouter>
   </React.StrictMode>
 );
