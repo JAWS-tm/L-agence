042fb8372ef553228e1ee638bbfef15e5e7a03c3
Hugo <hugo.dumartin@hotmail.fr>
2024-01-11 19:25:03 +0100

    fontend: design and logic for register view

--- packages/frontend/src/views/Register/Register.tsx
+++ packages/frontend/src/views/Register/Register.tsx
@@ -1,76 +1,121 @@
+import styles from './styles.module.scss';
 import { useNavigate } from 'react-router-dom';
 import useUserStore from '../../user/useUserStore';
 import { useState } from 'react';
 import { UserRegisteration, authService } from '../../services';
+import Input from '../../components/Input/Input';
+import Button from '../../components/Button/Button';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import toast, { Toaster } from 'react-hot-toast';
 
 type Props = {};
+
+const registerValidation = z
+  .object({
+    firstName: z.string().min(1, { message: 'Le prénom est requis.' }),
+    lastName: z.string().min(1, { message: 'Le nom est requis.' }),
+    email: z.string().min(1, { message: `L'email est requis.` }).email({
+      message: `L'email n'est pas valide.`,
+    }),
+    password: z.string().min(6, {
+      message: 'Le mot de passe doit contenir au moins 6 caractères.',
+    }),
+    confirmPassword: z
+      .string()
+      .min(1, { message: 'Le mot de passe de confirmation est requis.' }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'Les mots de passes ne correspondent pas.',
+  });
+
+type RegisterForm = z.infer<typeof registerValidation>;
 
 const Register = (props: Props) => {
   const navigate = useNavigate();
   const login = useUserStore((state) => state.login);
-  const [user, setUser] = useState<UserRegisteration | null>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterForm>({ resolver: zodResolver(registerValidation) });
+  const [loading, setLoading] = useState(false);
 
-  const handleValuesChange = (
-    field: keyof UserRegisteration,
-    value: string
-  ) => {
-    setUser((prevValues) => ({
-      ...prevValues!,
-      [field]: value,
-    }));
+  const handleRegister = (user: UserRegisteration) => {
+    setLoading(true);
+    authService
+      .register(user)
+      .then((res) => {
+        if (res.data.user) {
+          login();
+          navigate('/profile');
+        } else {
+          toast.error('Erreur.');
+        }
+      })
+      .catch((err) => {
+        if (err.response.data.status)
+          toast.error('Adresse mail déjà utilisée.');
+      })
+      .finally(() => setLoading(false));
   };
 
-  const handleRegister = async () => {
-    if (!user) {
-      console.log('some fields are empty');
-    } else if (user.password !== user.confirmPassword) {
-      console.log("passwords don't macth");
-    } else {
-      const isLogged = await authService.register(user);
-      if (isLogged) {
-        login();
-        navigate('/profile');
-      } else {
-        // handle wrong registeration
-      }
-    }
-  };
+  const onSubmit = handleSubmit((data) => {
+    return handleRegister(data);
+  });
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', width: 300 }}>
-      Register
-      <input
-        type="text"
-        placeholder="Nom"
-        onChange={(e) => handleValuesChange('firstName', e.target.value)}
-        value={user?.firstName}
-      />
-      <input
-        type="text"
-        placeholder="Prénom"
-        onChange={(e) => handleValuesChange('lastName', e.target.value)}
-        value={user?.lastName}
-      />
-      <input
-        type="email"
-        placeholder="email"
-        onChange={(e) => handleValuesChange('email', e.target.value)}
-        value={user?.email}
-      />
-      <input
-        type="password"
-        placeholder="mot de passe"
-        onChange={(e) => handleValuesChange('password', e.target.value)}
-        value={user?.password}
-      />
-      <input
-        type="password"
-        placeholder="mot de passe"
-        onChange={(e) => handleValuesChange('confirmPassword', e.target.value)}
-        value={user?.confirmPassword}
-      />
-      <button onClick={handleRegister}>S'incrire</button>
-      <button onClick={() => navigate('/login')}>Déjà un compte </button>
+    <div className={styles.login}>
+      <h1 className={styles.title}>Créer mon compte</h1>
+
+      <form onSubmit={onSubmit} className={styles.form}>
+        <Input
+          type="text"
+          placeholder="Prénom"
+          {...register('firstName')}
+          errorMsg={errors.firstName?.message}
+        />
+        <Input
+          type="text"
+          placeholder="Nom"
+          {...register('lastName')}
+          errorMsg={errors.lastName?.message}
+        />
+        <Input
+          type="email"
+          placeholder="Email"
+          {...register('email')}
+          errorMsg={errors.email?.message}
+        />
+        <Input
+          type="password"
+          placeholder="Mot de passe"
+          {...register('password')}
+          errorMsg={errors.password?.message}
+        />
+        <Input
+          type="password"
+          placeholder="Confirmer le mot de passe"
+          {...register('confirmPassword')}
+          errorMsg={errors.confirmPassword?.message}
+        />
+        <Button
+          type="primary"
+          actionType="submit"
+          value="Valider"
+          loading={loading}
+        />
+        <p className={styles.alreadyAccount}>
+          {' '}
+          Déjà un compte ?{' '}
+          <span className={styles.link} onClick={() => navigate('/login')}>
+            Se connecter
+          </span>
+        </p>
+      </form>
+      <Toaster />
     </div>
   );
 };
