1c6048b7830266c8c945b84f27d70a8de64368e6
Hugo <hugo.dumartin@hotmail.fr>
2024-01-12 11:46:17 +0100

    frontend: add navbar logo

--- packages/frontend/src/components/Navbar/Navbar.tsx
+++ packages/frontend/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import './navbar.scss';
 import Button from '../Button/Button';
+import logo from '../../assets/l-agence_logo.png';
 
 type Props = {};
 
@@ -9,11 +10,14 @@ const Navbar = (props: Props) => {
   return (
     <div className="navbar">
       <div className="logo" onClick={() => navigate('/')}>
-        L'agence
+        <img src={logo} alt="" />
+        <span className="name">L'agence</span>
       </div>
       <div className="links">
         <span className="item">Nos biens</span>
-        <span className="item" onClick={() => navigate('/contact')}>Contact</span>
+        <span className="item" onClick={() => navigate('/contact')}>
+          Contact
+        </span>
       </div>
       <Button
         value="Mon compte"
