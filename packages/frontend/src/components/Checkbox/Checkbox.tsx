133cab8ad2f14952858808f2ead1ab4f7bdadd19
Jules <jules.dempt@outlook.fr>
2024-01-21 20:12:56 +0100

    fix: input & checkbox props

--- packages/frontend/src/components/Checkbox/Checkbox.tsx
+++ packages/frontend/src/components/Checkbox/Checkbox.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import styles from './Checkbox.module.scss';
+import { forwardRef } from 'react';
 
 type Props = {
   checked?: boolean;
@@ -8,21 +9,26 @@ type Props = {
   className?: string;
 };
 
-const Checkbox = ({ checked, label, onCheck, className }: Props) => {
-  return (
-    <div className={classNames(styles.container, className)}>
-      <label>
-        <input
-          className={styles.input}
-          type="checkbox"
-          checked={checked}
-          onChange={() => onCheck && onCheck(!checked)}
-        />
-        <i className={classNames('fa-solid fa-check', styles.icon)} />
-        <span className={styles.label}>{label}</span>
-      </label>
-    </div>
-  );
-};
+const Checkbox = forwardRef<HTMLInputElement, Props>(
+  ({ checked, label, onCheck, className }, ref) => {
+    return (
+      <div className={classNames(styles.container, className)}>
+        <label>
+          <div style={{ position: 'relative' }}>
+            <input
+              ref={ref}
+              className={styles.input}
+              type="checkbox"
+              checked={checked}
+              onChange={() => onCheck && onCheck(!checked)}
+            />
+            <i className={classNames('fa-solid fa-check', styles.icon)} />
+          </div>
+          <span className={styles.label}>{label}</span>
+        </label>
+      </div>
+    );
+  }
+);
 
 export default Checkbox;
