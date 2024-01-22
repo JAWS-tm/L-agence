154ceb51d5f3247a0407fd76e7c98d09fd688d57
gui804 <141036380+gui8024@users.noreply.github.com>
2024-01-22 09:38:22 +0100

    Update prod config.ts

--- packages/frontend/src/utils/config.ts
+++ packages/frontend/src/utils/config.ts
@@ -4,8 +4,8 @@ const DEV_CONFIG = {
 };
 
 const PROD_CONFIG = {
-  API_URL: 'https://api.lagence.io/api',
-  PUBLIC_CONTENT_URL: 'https://api.lagence.io',
+  API_URL: 'http://192.168.56.10:223/api',
+  PUBLIC_CONTENT_URL: 'http://192.168.56.10:223',
 };
 
 export const CONFIG =
