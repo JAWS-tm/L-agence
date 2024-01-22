c08ddcacdaa110d5638a5189add6f95e65fc6150
gui804 <141036380+gui8024@users.noreply.github.com>
2024-01-22 09:59:15 +0100

    Update config.ts

--- packages/frontend/src/utils/config.ts
+++ packages/frontend/src/utils/config.ts
@@ -4,9 +4,9 @@ const DEV_CONFIG = {
 };
 
 const PROD_CONFIG = {
-  API_URL: 'http://192.168.56.10:223/api',
-  PUBLIC_CONTENT_URL: 'http://192.168.56.10:223',
+  API_URL: 'http://192.168.56.10:3000/api',
+  PUBLIC_CONTENT_URL: 'http://192.168.56.10:3000',
 };
 
-export const CONFIG =
+export const CONFIG =https://github.com/JAWS-tm/L-agence/edit/master/packages/frontend/src/utils/config.ts
   process.env.NODE_ENV === 'production' ? PROD_CONFIG : DEV_CONFIG;
